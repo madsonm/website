71cf5aa0debdd027e4df3d9680acff10f3d2330b
Madson <madson.work@gmail.com>
2023-11-11 02:57:24 -0600

    Routing updates

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule,Routes } from '@angular/router';
 
 const routes: Routes = [
-	{ path: '**',redirectTo: 'bb' }
+	{ path: '**',redirectTo: 'legendary' }
 ];
 
 @NgModule({
